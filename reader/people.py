9f929fe5c224015a072d5d682006797f561d0d4c
NoaRicky <480582562@qq.com>
2018-11-19 20:18:11 +0800

    walk through each file

--- reader/people.py
+++ reader/people.py
@@ -1,6 +1,7 @@
 from typing import Dict, List, Sequence, Iterable
 import logging
 import re
+import os
 
 from overrides import overrides
 
@@ -25,9 +26,21 @@ class PeopleReader(DatasetReader):
     @overrides
     def _read(self, file_path: str) -> Iterable[Instance]:
 
-        raise NotImplementedError
+        root_dir = file_path
+        logger.info(f"root dir is {root_dir}")
+        for dir_name, sub_list, file_list in os.walk(root_dir):
+            for file in file_list:
+                cur_file = os.path.join(dir_name, file)
+                logger.info(f"processing: {cur_file}")
+                with open(cur_file, mode='r', encoding='utf-8') as fp:
+                    for line in fp.readlines():
+                        yield self._process_line(line)
 
     @overrides
     def text_to_instance(self, *inputs) -> Instance:
+
+        raise NotImplementedError
+
+    def _process_line(self, line):
 
         raise NotImplementedError
