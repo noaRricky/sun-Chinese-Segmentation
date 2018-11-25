ac565f0a191051c6fbb53318759d57d5e24b8327
NoaRicky <480582562@qq.com>
2018-11-25 10:05:47 +0800

    modify store folder and remove useless file

--- experiment/simple.py
+++
@@ -1,8 +0,0 @@
-
-
-from my_library.reader import PeopleReader
-
-reader = PeopleReader()
-
-train_data = reader.read('../corpus/people/train.txt')
-valid_data = reader.read('../corpus/people/valid.txt')
--- my_train.py
+++ my_train.py
@@ -5,5 +5,5 @@ from my_library.reader import PeopleReader
 
 if __name__ == '__main__':
     params = Params.from_file('./experiment/simple.json')
-    serialization_dir = './store/simple'
-    train_model(params, serialization_dir, recover=True)
+    serialization_dir = './store/exp1'
+    train_model(params, serialization_dir, recover=False)
